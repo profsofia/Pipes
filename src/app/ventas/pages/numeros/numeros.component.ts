17aa11669873d43d5fc62ea53d6bad8f049a40c3
SofiaSchenone <sofiainesschenone@gmail.com>
2022-12-22 17:29:45 +0000

    numeros

--- src/app/ventas/pages/numeros/numeros.component.ts
+++ src/app/ventas/pages/numeros/numeros.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-numeros',
-  templateUrl: './numeros.component.html',
-  styleUrls: ['./numeros.component.css']
+  templateUrl: './numeros.component.html'
 })
 export class NumerosComponent {
-
+ventasNetas : number = 2904358973594591.6789;
+porcentaje : number = 0.47;
 }
